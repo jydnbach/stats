5cfd8b8ee673ea57a5b3ab7a81efc77c0da49137
jydnbach <jaydenkim95@gmail.com>
2024-07-18 15:47:32 +0900

    Summary class

--- src/Summary.ts
+++ src/Summary.ts
@@ -1,1 +1,15 @@
-export interface Analyzer {}
+import { MatchData } from './MatchData';
+
+export interface Analyzer {
+  run(matches: MatchData[]): string;
+}
+
+export interface OutputTarget {
+  print(report: string): void;
+}
+
+export class Summary {
+  constructor(public analyzer: Analyzer, public outputTarger: OutputTarget) {}
+}
+
+new Summary(new WinsAnalysis(), new ConsoleReport());
